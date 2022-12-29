a1104170c0ffa5ab9922cc4087792601aaae80af
Pawel Urasinski <uraspawel@gmail.com>
2022-12-29 23:30:33 +0100

    Changed the logic in the console interface
    Added removing of compressed folder, after skipping a downloaded recipe

--- integration/console-interface.js
+++ integration/console-interface.js
@@ -1,31 +1,33 @@
 const { createInterface } = require('readline');
 const { basename } = require('path');
 const { decryptFolder } = require('../utils/decrypt-from-zip');
+const { removeFolder } = require('../utils/rm-folder');
 
 const consoleInterface = createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
 const questionToUser = (dishName, dishId) => {
-  consoleInterface.question(
-    `Type ID of ${basename(
-      dishName
-    )} to confirm your choise and unzip the file or skip it by pres any keyboard key  \n`,
-    (answer) => {
-      if (answer === String(dishId)) {
-        decryptFolder(dishName);
-        console.log('You typed correct ID');
-        consoleInterface.close();
-      } else {
-        console.log('Try again and take the best dish for you!');
-        consoleInterface.close();
-      }
-    }
-  );
+	consoleInterface.question(
+		`Type ID of ${basename(
+			dishName
+		)} to confirm your choise and unzip the file or skip it by press any keyboard key  \n`,
+		answer => {
+			if (answer === String(dishId)) {
+				decryptFolder(dishName);
+				console.log('You typed correct ID');
+				consoleInterface.close();
+			} else {
+				removeFolder(dishName, '.tar', 1000);
+				console.log('Try again and take the best dish for you!');
+				consoleInterface.close();
+			}
+		}
+	);
 };
 
 module.exports = {
-  questionToUser,
-  consoleInterface,
+	questionToUser,
+	consoleInterface,
 };
