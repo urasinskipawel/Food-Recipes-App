46854ccd911e64cac729cb9314cd237f377f53d4
Pawel Urasinski <uraspawel@gmail.com>
2022-12-30 01:17:54 +0100

    Added special folder for downloaded recipes
    After launching the App, a folder will be created to store the  recipes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: '.env' });
 
 const { join } = require('path');
+const { mkdir } = require('fs').promises;
 const { apiConnection } = require('./config/api-config');
 const { questionToUser } = require('./integration/console-interface');
 const { encryptFolder } = require('./utils/encrypt-to-zip');
@@ -10,36 +11,37 @@ const { checkApiConnection } = require('./validators/check-api-connection');
 const { getDishData } = require('./utils/get-dish-data');
 
 (async () => {
-  try {
-    const userData = process.argv[2];
-    checkUserData(userData);
+	try {
+		const userData = process.argv[2];
+		checkUserData(userData);
 
-    const apiData = await apiConnection(userData);
-    checkApiConnection(apiData);
+		const apiData = await apiConnection(userData);
+		checkApiConnection(apiData);
 
-    const randomDishNumber = Math.floor(Math.random() * apiData.results.length);
-    const apiDataResult = apiData.results[randomDishNumber];
-    const dishData = {
-      id: apiDataResult.id,
-      name: apiDataResult.name,
-    };
+		const randomDishNumber = Math.floor(Math.random() * apiData.results.length);
+		const apiDataResult = apiData.results[randomDishNumber];
+		const dishData = {
+			id: apiDataResult.id,
+			name: apiDataResult.name,
+		};
 
-    console.log(`The ID of the drawn dish is: ${dishData.id}`);
+		console.log(`The ID of the drawn dish is: ${dishData.id}`);
 
-    const dishDirectory = join('./Random Recipes/', dishData.name);
-    checkFileExist(dishDirectory);
+		await mkdir('./Random Recipes/', { recursive: true });
+		const dishDirectory = join('./Random Recipes/', dishData.name);
+		checkFileExist(dishDirectory);
 
-    getDishData.getDishIngredients(apiDataResult, dishDirectory);
-    getDishData.getDishInstructions(apiDataResult, dishDirectory);
-    getDishData.getDishImage(apiDataResult, dishDirectory);
-    getDishData.getDishNutrients(apiDataResult, dishDirectory);
+		getDishData.getDishIngredients(apiDataResult, dishDirectory);
+		getDishData.getDishInstructions(apiDataResult, dishDirectory);
+		getDishData.getDishImage(apiDataResult, dishDirectory);
+		getDishData.getDishNutrients(apiDataResult, dishDirectory);
 
-    setTimeout(() => {
-      encryptFolder(dishDirectory);
-    }, 1000);
+		setTimeout(() => {
+			encryptFolder(dishDirectory);
+		}, 1000);
 
-    questionToUser(dishDirectory, dishData.id);
-  } catch (err) {
-    console.error(`Error has occured: ${err}`);
-  }
+		questionToUser(dishDirectory, dishData.id);
+	} catch (err) {
+		console.error(`Error has occured: ${err}`);
+	}
 })();
